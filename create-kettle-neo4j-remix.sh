55e5b5272a27586a6c886a75c2471fda2beb9c92
Matt Casters <mattcasters@gmail.com>
2018-11-11 17:42:31 +0100

    Adding plugins, updated remix

--- create-kettle-neo4j-remix.sh
+++ create-kettle-neo4j-remix.sh
@@ -1,8 +1,8 @@
 
-# set -ex
+set -ex
 
 BASE_FILE=~/software/pdi-ce-8.1.0.0-365.zip
-REMIX_VERSION=8.1.0.4
+REMIX_VERSION=8.1.0.4-REMIX
 REMIX_FILE=kettle-neo4j-remix-$REMIX_VERSION.zip
 SOFT_DIR=~/software
 TMP=/tmp
@@ -26,34 +26,54 @@ cp $SOFT_DIR/latest-patches/kettle-dbdialog-8.1.0.0-SNAPSHOT.jar $FOLDER/lib/ket
 cp $SOFT_DIR/latest-patches/kettle-engine-8.1.0.0-SNAPSHOT.jar $FOLDER/lib/kettle-engine-8.1.0.0-365.jar
 cp $SOFT_DIR/latest-patches/kettle-ui-swt-8.1.0.0-SNAPSHOT.jar $FOLDER/lib/kettle-ui-swt-8.1.0.0-365.jar
 
+cp getLatestSamples.sh $TMP
+cp getLatestBase.sh $TMP
+
 # get the latest samples
 #
-cp getLatestSamples.sh $TMP
 cd $TMP
 ./getLatestSamples.sh
-unzip -q kettle-plugin-examples.zip -d $FOLDER
+unzip -q -o kettle-plugin-examples.zip -d $FOLDER
 cd - > /dev/null
 
 # Latest Azure plugins
 #
-./getLatestAzurePlugins.sh
-unzip -q $TMP/kettle-azure-event-hubs-latest.zip -d $FOLDER/plugins
+./getLatestBase.sh mattcasters/kettle-azure-event-hubs kettle-azure-event-hubs
+unzip -q -o $TMP/kettle-azure-event-hubs-latest.zip -d $FOLDER/plugins
 
 # Latest Data Set plugins
 #
-./getLatestDataSetPlugin.sh
-unzip -q $TMP/pentaho-pdi-dataset-latest.zip -d $FOLDER/plugins
+./getLatestBase.sh mattcasters/pentaho-pdi-dataset pentaho-pdi-dataset
+unzip -q -o $TMP/pentaho-pdi-dataset-latest.zip -d $FOLDER/plugins
 
 # Latest Kettle debug plugin
 #
-./getLatestKettleDebugPlugin.sh
-unzip -q $TMP/kettle-debug-plugin-latest.zip -d $FOLDER/plugins
+./getLatestBase.sh mattcasters/kettle-debug-plugin kettle-debug-plugin
+unzip -q -o $TMP/kettle-debug-plugin-latest.zip -d $FOLDER/plugins
 
 # Latest Neo4j plugins
 #
-./getLatestNeo4jPlugins.sh
-unzip -q $TMP/Neo4jOutput-latest.zip -d $FOLDER/plugins
+./getLatestBase.sh knowbi/knowbi-pentaho-pdi-neo4j-output Neo4JOutput
+unzip -q -o $TMP/Neo4JOutput-latest.zip -d $FOLDER/plugins
 
+# Latest Kettle Neo4j Logging plugin
+#
+./getLatestBase.sh mattcasters/kettle-neo4j-logging kettle-neo4j-logging
+unzip -q -o $TMP/kettle-neo4j-logging-latest.zip -d $FOLDER/plugins
+
+# Latest needful things & install Maitre
+#
+./getLatestBase.sh mattcasters/kettle-needful-things kettle-needful-things
+unzip -q -o $TMP/kettle-needful-things-latest.zip -d $FOLDER/plugins
+cp $FOLDER/plugins/kettle-needful-things/Maitre.bat $FOLDER
+cp $FOLDER/plugins/kettle-needful-things/maitre.sh $FOLDER
+cp $FOLDER/plugins/kettle-needful-things/kettle-needful-things-*.jar $FOLDER/lib
+cp $FOLDER/plugins/kettle-needful-things/lib/picocli-*.jar $FOLDER/lib
+
+# The Environment plugin
+#
+./getLatestBase.sh mattcasters/kettle-environment kettle-environment
+unzip -q -o $TMP/kettle-environment-latest.zip -d $FOLDER/plugins
 
 # Now zip it back up...
 #
--- getLatestBase.sh
+++ getLatestBase.sh
@@ -1,8 +1,10 @@
 
-cd /tmp/
+set -ex
 
-PROJECT=mattcasters/kettle-debug-plugin
-FILE_BASE=kettle-debug-plugin
+cd /tmp
+
+PROJECT=$1
+FILE_BASE=$2
 
 VERSION=$( curl --silent "https://api.github.com/repos/$PROJECT/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' )
 ZIP=https://github.com/${PROJECT}/releases/download/$VERSION/${FILE_BASE}-${VERSION}.zip
@@ -13,3 +15,4 @@ curl --silent $ZIP -L -O
 
 rm -f ${FILE_BASE}-latest.zip
 mv ${FILE_BASE}-${VERSION}.zip ${FILE_BASE}-latest.zip
+
