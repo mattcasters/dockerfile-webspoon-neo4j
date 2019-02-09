0fb909c97a666e36734f86ece837d0e5bf92bc48
Ubuntu <sramazzina@seraci01.ielkupadszeexpgxsuoa0pig0a.parx.internal.cloudapp.net>
2019-02-09 13:59:26 +0000

    Light refatcoring of original scripts

--- getLatestSamples.sh
+++ getLatestSamples.sh
@@ -2,7 +2,7 @@
 FILE_BASE=kettle-plugin-examples
 rm -rf ${FILE_BASE}
 
-git clone git@github.com:neo4j-examples/kettle-plugin-examples.git 2> kettle-plugin-examples-git.log
+git clone https://github.com/neo4j-examples/kettle-plugin-examples.git 2> kettle-plugin-examples-git.log
 
 if [ -f ${FILE_BASE}.zip ]
 then
