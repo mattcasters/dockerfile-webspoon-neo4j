ff87cf6987b213bf19d1df9bc933439bad384fe8
Matt Casters <mattcasters@gmail.com>
2020-02-10 13:48:18 +0100

    Beam and SpoonGit updates

--- getLatestSpoonGit.sh
+++ getLatestSpoonGit.sh
@@ -4,14 +4,16 @@
 # https://github.com/HiromuHota/pdi-git-plugin/releases/download/1.0.0/pdi-git-plugin-1.0.0-jar-with-dependencies.zip
 # https://github.com/HiromuHota/pdi-git-plugin/releases/download/nightly/pdi-git-plugin-1.0.2-SNAPSHOT-jar-with-dependencies.zip
 # https://github.com/HiromuHota/pdi-git-plugin/releases/download/nightly/pdi-git-plugin-1.1.0-SNAPSHOT-jar-with-dependencies.zip
+# https://github.com/HiromuHota/pdi-git-plugin/releases/download/nightly/pdi-git-plugin-1.1.0-SNAPSHOT-jar-with-dependencies.zip
+# https://github.com/HiromuHota/pdi-git-plugin/releases/download/1.1.0/pdi-git-plugin-1.1.0-jar-with-dependencies.zip
 
 cd /tmp
 
 PROJECT=HiromuHota/pdi-git-plugin
 FILE_BASE=pdi-git-plugin
 
-ZIP="https://github.com/HiromuHota/pdi-git-plugin/releases/download/nightly/pdi-git-plugin-1.1.0-SNAPSHOT-jar-with-dependencies.zip"
-FILE=pdi-git-plugin-1.1.0-SNAPSHOT-jar-with-dependencies.zip
+ZIP="https://github.com/HiromuHota/pdi-git-plugin/releases/download/1.1.0/pdi-git-plugin-1.1.0-jar-with-dependencies.zip"
+FILE=pdi-git-plugin-1.1.0-jar-with-dependencies.zip
 
 echo $PROJECT version 1.1.0
 
